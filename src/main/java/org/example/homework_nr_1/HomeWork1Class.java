c679c52336ee7e0d2d3c8f548048d2590d1dabe1
Serghei_Drangoz <sereja04031997@gmail.com>
2023-03-19 20:22:51 +0000

    Initial Commit

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1Class.java
@@ -0,0 +1,8 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1Class {
+            public static void main(String[] args) {
+            System.out.println("This is  my first homework!");
+        }
+
+}
